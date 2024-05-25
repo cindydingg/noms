a0faea90f0d695449a7e5e155354c6617ca52e7e
cindydingg <87918262+cindydingg@users.noreply.github.com>
2024-05-24 23:29:51 -0700

    merged addition of recipe detail screen

--- screens/RecipeDetailScreen.js
+++ screens/RecipeDetailScreen.js
@@ -1,39 +1,27 @@
-import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const RecipeDetailScreen = ({ route }) => {
-  const { name, date, image } = route.params;
-
+const RecipeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Image source={image} style={styles.image} />
-      <Text style={styles.name}>{name}</Text>
-      <Text style={styles.date}>{date}</Text>
-      {/* Add more details about the recipe here */}
+      <Text style={styles.headerText}>Spaghetti with Marinara Sauce</Text>
+      <Image source={require('../assets/spaghettiphoto.jpeg')} style={styles.image} />
+      <View style={styles.iconsContainer}>
+        <Text style={styles.iconText}>🥗 Vegetarian</Text>
+        <Text style={styles.iconText}>🌱 Vegan</Text>
+      </View>
+      <Text style={styles.subHeaderText}>Ingredients:</Text>
+      <Text style={styles.ingredientText}>• Spaghetti Pasta Noodles</Text>
+      <Text style={styles.ingredientText}>• Marinara Sauce</Text>
+      <Text style={styles.ingredientText}>• Garlic</Text>
+      <Text style={styles.ingredientText}>• Bell Pepper</Text>
+      <View style={styles.inStockContainer}>
+        <Text style={styles.inStockText}>In Stock</Text>
+      </View>
+      <Text style={styles.subHeaderText}>Steps:</Text>
+      <View style={styles.stepContainer}>
+        <Text style={styles.stepNumber}>1</Text>
+        <Text style={styles.stepText}>Chop vegetables</Text>
+      </View>
+      <Text style={styles.stepDescription}>Peel and finely chop garlic, cut bell pepper in half.</Text>
+      <Image source={require('../assets/chopping.jpeg')} style={styles.stepImage} />
     </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 20,
-  },
-  image: {
-    width: '100%',
-    height: 200,
-    borderRadius: 10,
-    marginBottom: 20,
-  },
-  name: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  date: {
-    fontSize: 16,
-    color: '#666',
-  },
-});
-
-export default RecipeDetailScreen;
